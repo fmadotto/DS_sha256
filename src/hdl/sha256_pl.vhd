d305ebe6e1fe48dd906065a9066654f2a944e6c3
Federico Madotto <federico.madotto@gmail.com>
2016-05-20 12:18:35 +0200

    implementation of the PL completed. To be tested

--- src/hdl/sha256_pl.vhd
+++ src/hdl/sha256_pl.vhd
@@ -73,13 +73,6 @@ end entity sha256_pl;
 
 architecture rtl of sha256_pl is
 
-  -- Record versions of AXI signals
-  signal s0_axi_m2s: axilite_gp_m2s;
-  signal s0_axi_s2m: axilite_gp_s2m;
-
-  -- STATUS register
-  signal status: std_ulogic_vector(31 downto 0);
-
   -- Or reduction of std_ulogic_vector
   function or_reduce(v: std_ulogic_vector) return std_ulogic is
     variable tmp: std_ulogic_vector(v'length - 1 downto 0) := v;
@@ -101,13 +94,14 @@ architecture rtl of sha256_pl is
       address_size  : natural := 4
     );
     port (
-      clk           : in  std_ulogic; -- clock
-      rstn          : in  std_ulogic; -- asynchronous active low reset
-      cs_n          : in  std_ulogic; -- chip select: when asserted low, memory read and write operations are possible
-      we_n          : in  std_ulogic; -- write enable: when asserted low, memory can be written
-      address       : in  std_ulogic_vector(address_size-1 downto 0);
-      data_in       : in  std_ulogic_vector(row_size-1 downto 0);
-      data_out      : out std_ulogic_vector(row_size-1 downto 0)
+      clk      : in  std_ulogic; -- clock
+      rcs_n    : in  std_ulogic; -- read chip select: when asserted low, memory can be read
+      wcs_n    : in  std_ulogic; -- write chip select: when asserted low, memory can be written if also we_n is low
+      we_n     : in  std_ulogic; -- write enable: when asserted low, memory can be written
+      r_addr   : in  std_ulogic_vector(address_size-1 downto 0);
+      w_addr   : in  std_ulogic_vector(address_size-1 downto 0);
+      data_in  : in  std_ulogic_vector(row_size-1 downto 0);
+      data_out : out std_ulogic_vector(row_size-1 downto 0)
     );
   end component M_j_memory;
 
@@ -130,54 +124,27 @@ architecture rtl of sha256_pl is
       exp_sel1_delayed    : out std_ulogic; -- select signal for exp_mux1
       com_sel1_delayed    : out std_ulogic; -- select signal for exp_mux1
 
-      -- M_j_memory ports
-      M_j_memory_cs_n     : out  std_ulogic; -- chip select: when asserted low, memory read and write operations are possible
-      M_j_memory_we_n     : out  std_ulogic; -- write enable: when asserted low, memory can be written
-      M_j_memory_address  : out  std_ulogic_vector(3 downto 0);
+      -- M_j_memory_dual ports
+      M_j_memory_rcs_n    : out std_ulogic; -- read chip select: when asserted low, memory can be read
+      M_j_memory_r_addr   : out std_ulogic_vector(3 downto 0);
 
       -- reg_H_minus_1 ports
-      reg_H_minus_1_en    : out  std_ulogic; -- enable signal for the H(i-1) registers
-      reg_H_minus_1_sel   : out  std_ulogic; -- select signal for the H(i-1) registers
+      reg_H_minus_1_en    : out std_ulogic; -- enable signal for the H(i-1) registers
+      reg_H_minus_1_sel   : out std_ulogic; -- select signal for the H(i-1) registers
       
       -- K_j_constants ports
-      K_j_init            : out  std_ulogic -- start signal for K_j
+      K_j_init            : out std_ulogic -- start signal for K_j
     );
   end component control_unit;
 
-  component data_path is
+  component K_j_constants is
     port (
-      -- common ports
-      clk          : in  std_ulogic;                     -- clock
-      rstn         : in  std_ulogic;                     -- asynchronous active low reset
-
-      -- expander input ports
-      exp_sel1     : in  std_ulogic;                     -- select signal for exp_mux1
-      M_i_j        : in  std_ulogic_vector(31 downto 0); -- 32-bit word of the i-th message block
-
-      -- compressor input ports
-      com_sel1     : in  std_ulogic;                     -- select signal for com_muxA, ..., com_muxH
-      W_i_j        : in  std_ulogic_vector(31 downto 0); -- 32-bit W_i_j
-      K_j          : in  std_ulogic_vector(31 downto 0); -- NIST-defined constants Kj
-      H_iminus1_A,
-      H_iminus1_B,
-      H_iminus1_C,
-      H_iminus1_D,
-      H_iminus1_E,
-      H_iminus1_F,
-      H_iminus1_G,
-      H_iminus1_H  : in  std_ulogic_vector(31 downto 0); -- intermediate hash value H_(i-1)
-
-      -- output ports
-      H_i_A,
-      H_i_B,
-      H_i_C,
-      H_i_D,
-      H_i_E,
-      H_i_F,
-      H_i_G,
-      H_i_H        : out std_ulogic_vector(31 downto 0)  -- resulting hash value H_(i)
+      clk      : in  std_ulogic; -- clock
+      rstn     : in  std_ulogic; -- asynchronous active low reset
+      K_j_init : in  std_ulogic; -- start signal for K_j
+      K_j      : out std_ulogic_vector(31 downto 0) -- NIST-defined constants Kj
     );
-  end component data_path;
+  end component K_j_constants;
 
   component reg_H_minus_1 is
     port (
@@ -204,22 +171,72 @@ architecture rtl of sha256_pl is
     );
   end component reg_H_minus_1;
 
+  component data_path is
+    port (
+      -- common ports
+      clk          : in  std_ulogic;                     -- clock
+      rstn         : in  std_ulogic;                     -- asynchronous active low reset
+
+      -- expander input ports
+      exp_sel1     : in  std_ulogic;                     -- select signal for exp_mux1
+      M_i_j        : in  std_ulogic_vector(31 downto 0); -- 32-bit word of the i-th message block
+
+      -- compressor input ports
+      com_sel1     : in  std_ulogic;                     -- select signal for com_muxA, ..., com_muxH
+      K_j          : in  std_ulogic_vector(31 downto 0); -- NIST-defined constants Kj
+      H_iminus1_A,
+      H_iminus1_B,
+      H_iminus1_C,
+      H_iminus1_D,
+      H_iminus1_E,
+      H_iminus1_F,
+      H_iminus1_G,
+      H_iminus1_H  : in  std_ulogic_vector(31 downto 0); -- intermediate hash value H_(i-1)
+
+      -- output ports
+      H_i_A,
+      H_i_B,
+      H_i_C,
+      H_i_D,
+      H_i_E,
+      H_i_F,
+      H_i_G,
+      H_i_H        : out std_ulogic_vector(31 downto 0)  -- resulting hash value H_(i)
+    );
+  end component data_path;
+
   -- signals
+
+  -- Record versions of AXI signals
+  signal s0_axi_m2s : axilite_gp_m2s;
+  signal s0_axi_s2m : axilite_gp_s2m;
+
+  -- STATUS register
+  signal status : std_ulogic_vector(31 downto 0);
+
   -- M_j_memory
-  signal M_j_memory_data_out : std_ulogic_vector(31 downto 0);
+  signal M_j_memory_wcs_n_in,
+         M_j_memory_we_n_in   : std_ulogic;
+  signal M_j_memory_w_addr_in : std_ulogic_vector(3 downto 0);
+  signal M_j_memory_data_in   : std_ulogic_vector(31 downto 0);
+  signal M_j_memory_data_out  : std_ulogic_vector(31 downto 0);
+
   
   -- start_FF
+  signal start_FF_start_in  : std_ulogic;
   signal start_FF_start_out : std_ulogic;
 
   -- control_unit
   signal cu_exp_sel1_delayed_out    : std_ulogic; 
   signal cu_com_sel1_delayed_out    : std_ulogic; 
-  signal cu_M_j_memory_cs_n_out     : std_ulogic; 
-  signal cu_M_j_memory_we_n_out     : std_ulogic; 
-  signal cu_M_j_memory_address_out  : std_ulogic_vector(3 downto 0);
+  signal cu_M_j_memory_rcs_n_out    : std_ulogic; 
+  signal cu_M_j_memory_r_addr_out   : std_ulogic_vector(3 downto 0);
   signal cu_reg_H_minus_1_en_out    : std_ulogic; 
   signal cu_reg_H_minus_1_sel_out   : std_ulogic; 
   signal cu_K_j_init_out            : std_ulogic;
+
+  -- K_j_constants
+  signal K_j_constants_out : std_ulogic_vector(31 downto 0);
   
   -- data_path
   signal dp_H_i_A_out,
@@ -243,6 +260,117 @@ architecture rtl of sha256_pl is
 
 begin
 
+  pl_M_j_memory1 : M_j_memory
+    generic map (
+      row_size      => 32,
+      address_size  => 4
+    )
+    port map (
+      clk      => clk,
+      rcs_n    => cu_M_j_memory_rcs_n_out,
+      wcs_n    => M_j_memory_wcs_n_in,
+      we_n     => M_j_memory_we_n_in,
+      r_addr   => cu_M_j_memory_r_addr_out,
+      w_addr   => M_j_memory_w_addr_in,
+      data_in  => M_j_memory_data_in,
+      data_out => M_j_memory_data_out
+    );
+
+  pl_start_FF1 : start_FF
+    port map (
+      clk   => clk,
+      d     => start_FF_start_in,
+      start => start_FF_start_out
+    );
+
+  pl_control_unit1 : control_unit
+    port map (
+      clk                 => clk,
+      rstn                => rstn,
+      start               => start_FF_start_out,
+      done                => done,
+      
+      -- data path ports
+      exp_sel1_delayed    => cu_exp_sel1_delayed_out,
+      com_sel1_delayed    => cu_com_sel1_delayed_out,
+
+      -- M_j_memory ports
+      M_j_memory_rcs_n    => cu_M_j_memory_rcs_n_out,
+      M_j_memory_r_addr   => cu_M_j_memory_r_addr_out,
+
+      -- reg_H_minus_1 ports
+      reg_H_minus_1_en    => cu_reg_H_minus_1_en_out,
+      reg_H_minus_1_sel   => cu_reg_H_minus_1_sel_out,
+      
+      -- K_j_constants ports
+      K_j_init            => cu_K_j_init_out
+    );
+
+  pl_K_j_constants1 : K_j_constants
+    port map (
+      clk      => clk,
+      rstn     => rstn,
+      K_j_init => cu_K_j_init_out,
+      K_j      => K_j_constants_out
+    );
+
+  pl_reg_H_minus_11 : reg_H_minus_1
+    port map (
+      clk               => clk,
+      rstn              => rstn,
+      reg_H_minus_1_en  => cu_reg_H_minus_1_en_out,
+      reg_H_minus_1_sel => cu_reg_H_minus_1_sel_out,
+      H_i_A             => dp_H_i_A_out,
+      H_i_B             => dp_H_i_B_out,
+      H_i_C             => dp_H_i_C_out,
+      H_i_D             => dp_H_i_D_out,
+      H_i_E             => dp_H_i_E_out,
+      H_i_F             => dp_H_i_F_out,
+      H_i_G             => dp_H_i_G_out,
+      H_i_H             => dp_H_i_H_out,
+      H_iminus1_A       => reg_H_iminus1_A_out,
+      H_iminus1_B       => reg_H_iminus1_B_out,
+      H_iminus1_C       => reg_H_iminus1_C_out,
+      H_iminus1_D       => reg_H_iminus1_D_out,
+      H_iminus1_E       => reg_H_iminus1_E_out,
+      H_iminus1_F       => reg_H_iminus1_F_out,
+      H_iminus1_G       => reg_H_iminus1_G_out,
+      H_iminus1_H       => reg_H_iminus1_H_out
+    );
+
+  pl_data_path1 : data_path
+    port map (
+      -- common ports
+      clk          => clk,
+      rstn         => rstn,
+
+      -- expander input ports
+      exp_sel1     => cu_exp_sel1_delayed_out,
+      M_i_j        => M_j_memory_data_out,
+
+      -- compressor input ports
+      com_sel1     => cu_com_sel1_delayed_out,
+      K_j          => K_j_constants_out,
+      H_iminus1_A  => reg_H_iminus1_A_out,
+      H_iminus1_B  => reg_H_iminus1_B_out,
+      H_iminus1_C  => reg_H_iminus1_C_out,
+      H_iminus1_D  => reg_H_iminus1_D_out,
+      H_iminus1_E  => reg_H_iminus1_E_out,
+      H_iminus1_F  => reg_H_iminus1_F_out,
+      H_iminus1_G  => reg_H_iminus1_G_out,
+      H_iminus1_H  => reg_H_iminus1_H_out,
+
+      -- output ports
+      H_i_A        => dp_H_i_A_out,
+      H_i_B        => dp_H_i_B_out,
+      H_i_C        => dp_H_i_C_out,
+      H_i_D        => dp_H_i_D_out,
+      H_i_E        => dp_H_i_E_out,
+      H_i_F        => dp_H_i_F_out,
+      H_i_G        => dp_H_i_G_out,
+      H_i_H        => dp_H_i_H_out
+    );
+
   -- S0_AXI read-write requests
   s0_axi_pr: process(clk, rstn)
     -- idle: waiting for AXI master requests: when receiving write address and data valid (higher priority than read), perform the write, assert write address
@@ -255,51 +383,71 @@ begin
   begin
     if rstn = '0' then
       s0_axi_s2m <= (rdata => (others => '0'), rresp => axi_resp_okay, bresp => axi_resp_okay, others => '0');
+      M_j_memory_wcs_n_in <= '1';
+      M_j_memory_we_n_in <= '1';
+      start_FF_start_in <= '0';
       state := idle;
     elsif clk'event and clk = '1' then
       -- s0_axi write and read
       case state is
         when idle =>
           if s0_axi_m2s.awvalid = '1' and s0_axi_m2s.wvalid = '1' then -- Write address and data
-            if or_reduce(s0_axi_m2s.awaddr(31 downto 3)) /= '0' then -- If unmapped address
+            if or_reduce(s0_axi_m2s.awaddr(31 downto 7)) /= '0' then -- If unmapped address
               s0_axi_s2m.bresp <= axi_resp_decerr;
-            elsif s0_axi_m2s.awaddr(2) = '0' then -- If read-only status register
+            elsif s0_axi_m2s.awaddr(7 downto 0) = x"00" then -- If read-only status register
               s0_axi_s2m.bresp <= axi_resp_slverr;
             else
               s0_axi_s2m.bresp <= axi_resp_okay;
-              for i in 0 to 3 loop
-                if s0_axi_m2s.wstrb(i) = '1' then
-                  r(8 * i + 7 downto 8 * i) <= s0_axi_m2s.wdata(8 * i + 7 downto 8 * i);
-                end if;
-              end loop;
+
+              if s0_axi_m2s.awaddr(7 downto 0) >= x"04" and s0_axi_m2s.awaddr(7 downto 0) < x"44" then -- If M_j memory
+
+                for i in 0 to 3 loop
+                  if s0_axi_m2s.wstrb(i) = '1' then
+                    M_j_memory_data_in(8 * i + 7 downto 8 * i) <= s0_axi_m2s.wdata(8 * i + 7 downto 8 * i);
+                  end if;
+                end loop;
+
+                -- every row in the memory can contain 32 bits, while the input address refers to byte:
+                -- it is first divided by 4 (i.e. 2-bit right shift), then 1 is subtracted (the first address is the status register)
+                -- then it is converted back to a ulogic_vector
+                M_j_memory_w_addr_in <= std_ulogic_vector(to_unsigned((to_integer(unsigned("00" & s0_axi_m2s.awaddr(7 downto 2))) - 1), 4));
+                M_j_memory_wcs_n_in <= '0';
+                M_j_memory_we_n_in <= '0';
+
+              elsif s0_axi_m2s.awaddr(7 downto 0) = x"44" then -- start command
+                start_FF_start_in <= '1';
+              end if;
+
             end if;
             s0_axi_s2m.awready <= '1';
             s0_axi_s2m.wready <= '1';
             s0_axi_s2m.bvalid <= '1';
             state := w1;
+
           elsif s0_axi_m2s.arvalid = '1' then
-            if or_reduce(s0_axi_m2s.araddr(31 downto 3)) /= '0' then -- If unmapped address
+            if or_reduce(s0_axi_m2s.araddr(31 downto 0)) /= '0' then -- If not status register
               s0_axi_s2m.rdata <= (others => '0');
               s0_axi_s2m.rresp <= axi_resp_decerr;
             else
               s0_axi_s2m.rresp <= axi_resp_okay;
-              if s0_axi_m2s.araddr(2) = '0' then -- If status register
-                s0_axi_s2m.rdata <= status;
-              else
-                s0_axi_s2m.rdata <= r;
-              end if;
+              s0_axi_s2m.rdata <= status;
             end if;
             s0_axi_s2m.arready <= '1';
             s0_axi_s2m.rvalid <= '1';
             state := r1;
           end if;
+
         when w1 =>
           s0_axi_s2m.awready <= '0';
           s0_axi_s2m.wready <= '0';
+          M_j_memory_wcs_n_in <= '1';
+          M_j_memory_we_n_in <= '1';
+          start_FF_start_in <= '0';
           if s0_axi_m2s.bready = '1' then
             s0_axi_s2m.bvalid <= '0';
             state := idle;
           end if;
+
         when r1 =>
           s0_axi_s2m.arready <= '0';
           if s0_axi_m2s.rready = '1' then
