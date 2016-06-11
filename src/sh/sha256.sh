e6faa484621655fa91aaaba91a61526489b01102
Federico Madotto <federico.madotto@gmail.com>
2016-06-11 13:52:34 +0200

    license updated

--- src/sh/sha256.sh
+++ src/sh/sha256.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# Copyright (c) 2016 Federico Madotto and Coline Doebelin
+# federico.madotto (at) gmail.com
+# coline.doebelin (at) gmail.com
+# https://github.com/fmadotto/DS_sha256
+
+# sha256.sh is part of DS_sha256.
+
+# DS_sha256 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# DS_sha256 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 if [ $# -eq 0 ]; then
     echo "[x] ERROR: Provide as input a string message which length must be less than 447 bits (max 55 characters)"
     exit 1
