2277c4cf299ce8cb237d80bef048a8303586115a
Federico Madotto <federico.madotto@gmail.com>
2016-06-11 13:53:23 +0200

    license updated

--- utils/sha_test.py
+++ utils/sha_test.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python
 
+# Copyright (c) 2016 Federico Madotto and Coline Doebelin
+# federico.madotto (at) gmail.com
+# coline.doebelin (at) gmail.com
+# https://github.com/fmadotto/DS_sha256
+
+# sha_test.py is part of DS_sha256.
+
+# DS_sha256 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# DS_sha256 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import sys
 
 # Initialise hash values
